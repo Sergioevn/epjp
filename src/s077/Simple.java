297abb728c8aba9845000c4c6b6dd8015bee726c
Administrator <Administrator@192.168.20.53>
2019-09-23 17:48:13 +0200

    23/09 Latest

--- src/s077/Simple.java
+++ src/s077/Simple.java
@@ -7,4 +7,35 @@ public class Simple {
 		}
 		return -value;
 	}
+
+	public static void main(String[] args) {
+		Simple simple = new Simple();
+		System.out.println("Negate " + Integer.MIN_VALUE);
+		try {
+			int x = simple.negate2(Integer.MIN_VALUE);
+			System.out.println("My value negated is: " + x);
+		} catch (MyNegateException mne) {
+			System.out.println("My value negated is close to: " + mne.getAlternativeValue());
+		}
+	}
+
+	public int negate2(int value) throws MyNegateException {
+		if (value == Integer.MIN_VALUE) {
+			throw new MyNegateException(Integer.MAX_VALUE);
+		}
+		return -value;
+	}
+}
+
+class MyNegateException extends Exception {
+	private int alternativeValue;
+
+	public MyNegateException(int alternativeValue) {
+		this.alternativeValue = alternativeValue;
+
+	}
+
+	public int getAlternativeValue() {
+		return alternativeValue;
+	}
 }
