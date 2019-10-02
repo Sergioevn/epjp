05ddd3be5e45a4c63107bc127126eb19e87ca641
Administrator <Administrator@192.168.20.53>
2019-10-02 17:55:11 +0200

    0212f

--- scripts/mySql/ME.sql
+++ scripts/mySql/ME.sql
@@ -71,13 +71,52 @@ order by 3,2,1;
 
 create or replace view phone_view as
     select first_name, last_name, phone_number
-    where first_name not in(
-    select distinct m.first_name 
-    
-    from hr.employees e join hr.employees m
-    on (e.manager_id=m.employee_id));
-
+    from HR.employees;
+   
 select *
     from phone_view;
 
 drop view phone_view;
+
+declare
+cursor v_phone_cursor is
+select last_name, phone_number, first_name 
+from phone_view
+where last_name like 'A%' or first_name like 'A%'
+order by 3,1
+;
+begin
+for v_cur in v_phone_cursor loop
+dbms_output.put_line(
+'' || v_cur.first_name || ' ' || v_cur.last_name || '.
+Phone: ' || v_cur.phone_number || '
+');
+
+end loop;
+end;
+/
+
+create or replace procedure get_coder_salary(
+p_coder_id in coders.coder_id%type,
+p_salary out coders.salary%type) is
+begin
+select salary
+into p_salary
+from coders
+where coder_id = p_coder_id;
+end get_coder_salary;
+/
+
+drop procedure get_coder_salary;
+
+declare
+v_id coders.coder_id%type := 105;
+v_salary coders.salary%type;
+begin
+get_coder_salary(v_id, v_salary);
+dbms_output.put_line('Salary is ' || v_salary);
+exception
+when others then
+dbms_output.put_line('Can''t get salary for ' || v_id);
+end;
+/
